ea73c370213ad8e1322433d6bcca7a317ad12eb2
Tim Weber <scy@scytale.name>
2010-10-25 21:51:31 +0200

    Let’s see whether I get that Jekyll thing

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php require_once '_head.php'; ?>
-<article><h2>Ein Beispieleintrag</h2>
-<div class="meta">Nathalie am 1.2.2003</div>
-<p>
-	Ich schreib hier jetzt mal irgendwelchen Kram.
-</p>
-</article>
-<?php require_once '_foot.php'; ?>
